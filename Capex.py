be63326f6be1264a52d5fffd08042c034d9a2960
Torono909 <86355565+Torono909@users.noreply.github.com>
2022-03-05 10:11:57 +0530

    Add files via upload

---
+++ Capex.py
@@ -0,0 +1,99 @@
+############################
+# CONSTANTS (IMPORTANT)
+############################
+
+DIGITS = '0123456789'
+
+############################
+# ERRORS (IMPORTANT)
+############################
+class Error:
+	def __init__(self, error_name, details):
+		self.error_name = error_name
+		self.details = details
+
+	def as_string(self):
+
+############################
+# TOKENS (IMPORTANT)            
+############################
+
+TT_INT = 'TT_INT'
+TT_FLOAT = 'FLOAT'
+TT_PLUS = 'PLUS'
+TT_MINUS = 'MINUS'
+TT_MUL = 'MUL'
+TT_DIV = 'DIV'
+TT_LPAREN = 'LPAREN'
+TT_RPAREN = 'RPAREN'
+
+
+class Token:
+	def __init__(self, type_, value):
+		self.type = type_
+		self.value = value
+
+	def __repr__(self):
+		if self.value: return f'{self.type}:{self.type}'
+		return f'{self.type}'
+
+#######################
+# LEXER               #
+#######################
+
+class Lexer:
+	def __init__(self, text):
+		self.text = text
+		self.pos = -1
+		self.current_char = None
+		self.advance()
+
+	def advance(self):
+		self.pos += 1
+		self.current_char = self.text[pos] if self.pos < len(self.text) else None
+
+	def make_tokens(self):
+		tokens = []
+
+		while self.current_char != None:
+			if self.current_char in '\t':
+				self.advance()
+			elif self.current_char in DIGITS:
+				tokens.append(self.make_number())
+			elif self.current_char == '+':
+				tokens.append(Token(TT_PLUS))
+				self.advance()
+			elif self.current_char == '-':
+				tokens.append(Token(TT_MINUS))
+				self.advance()
+			elif self.current_char == '*':
+				tokens.append(Token(TT_MUL))
+				self.advance()
+			elif self.current_char == '/':
+				tokens.append(Token(TT_DIV))
+				self.advance()
+			elif self.current_char == '(':
+				tokens.append(Token(TT_LPAREN))
+				self.advance()
+			elif self.current_char == ')':
+				tokens.append(Token(TT_RPAREN))
+				self.advance()
+			else:
+
+		return tokens
+
+	def make_number():
+		num_str = ''
+		dot_count = 0
+		while self.current_char != None and self.current_char in DIGITS + '.':
+			if current_char == '.':
+				if dot_count == '1': break
+				dot_count += 1
+				num_str += '.'
+			else:
+				num_str += self.current_char
+
+		if dot_count == 0:
+			return Token(TT_INT, int(num_str))
+		else:
+			return Token(TT_FLOAT, float(num_str))
